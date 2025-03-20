357c57209d499faa16608617abd0e705bd5b27f7
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-03-20 04:38:02 -0500

    style: add content to footer

--- src/components/HeaderAndFooter/Footer/Footer.jsx
+++ src/components/HeaderAndFooter/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <footer className="footer">Footer Example</footer>;
+  return <footer className={styles.footer}>Fake Store &copy;</footer>;
 }
